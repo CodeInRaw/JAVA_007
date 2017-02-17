dd63fa9f00951a8c338c64556b1abb48abaf3ab3
Ihor <ihor.ruskykh@gmail.com>
2017-02-17 19:33:08 +0200

    ex2

---
+++ JAV07_Luxoft/src/greet/Greeting.java
@@ -0,0 +1,5 @@
+package greet;
+
+public class Greeting{
+	public String text = "world";
+}
---
+++ JAV07_Luxoft/src/test/TestGreeting.java
@@ -0,0 +1,9 @@
+package test;
+import greet.Greeting;
+
+public class TestGreeting {
+	public static void main(String[] args) {
+		Greeting g = new Greeting();
+		System.out.println("Hello, " + g.text);
+	}
+}
