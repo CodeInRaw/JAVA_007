9fbf3db85787af18a2065884d1d67b8dfa5828f4
Ihor <ihor.ruskykh@gmail.com>
2017-02-20 19:41:41 +0200

    lesson2

---
+++ JVA-007/Programs/jva007-java-fundamentals/src/com/luxoft/jva007/ArraysExample.java
@@ -0,0 +1,10 @@
+package com.luxoft.jva007;
+
+/**
+ * Created by Ihor Ruskykh on 2/20/2017.
+ */
+public class ArraysExample {
+    public static void main (String[] args){
+        int[][] ints = new int[3][5];
+    }
+}
