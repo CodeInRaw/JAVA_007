0df6c456984cf976ad93e9c2a5b4bb97b7c1254a
Ihor <ihor.ruskykh@gmail.com>
2017-02-22 17:19:23 +0200

    get and set

---
+++ JVA-007/Programs/jva007-java-fundamentals/src/com/luxoft/jva007/GetterSetterExample.java
@@ -0,0 +1,18 @@
+package com.luxoft.jva007;
+
+/**
+ * Created by Ihor Ruskykh on 2/22/2017.
+ */
+public class GetterSetterExample {
+    private String name;
+
+    // get access to the name value
+    public String getName(){
+        return name;
+    }
+
+    // set the amount for name value
+    public void setName(String name){
+        this.name = name;
+    }
+}
